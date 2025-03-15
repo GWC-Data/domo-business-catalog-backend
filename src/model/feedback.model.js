6816d66f8568575e11d9ccc523f4386598118592
vishwanath231-GWC <vishwanath.a@gwcdata.ai>
2025-03-15 11:03:40 +0530

    feedback schema

--- src/model/feedback.model.js
+++ src/model/feedback.model.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const feedbackSchema = mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required:  true,
-      ref: 'User'
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
     },
     comment: {
       type: String,
